6a198a93d90bc30d5114045f3ccd13e7d0d02794
wellmtx <wellingtonsaraivasousa@gmail.com>
2023-08-03 01:52:13 -0300

    feat: :wrench: envs configuration

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -20,6 +20,8 @@ import rateLimiter from "./middlewares/rateLimiter";
 createConnection();
 const app = express();
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.use(rateLimiter);
 
 Sentry.init({
@@ -36,8 +38,6 @@ app.use(Sentry.Handlers.tracingHandler());
 
 app.use(express.json());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
@@ -46,6 +46,7 @@ app.use(router);
 
 app.use(Sentry.Handlers.errorHandler());
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof AppError) {
     return res.status(err.statusCode).json({
--- src/shared/infra/typeorm/data-source.ts
+++ src/shared/infra/typeorm/data-source.ts
@@ -1,22 +1,23 @@
 import "reflect-metadata";
+import "dotenv/config";
 import { DataSource } from "typeorm";
 
 const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 4002,
-  username: "docker",
-  password: "6O8j5L75",
-  database: "rentx",
+  host: process.env.DATABASE_HOST,
+  port: 5432,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_DB,
   synchronize: false,
   logging: false,
-  entities: [],
+  entities: ["./src/modules/**/infra/typeorm/entities/*.ts"],
   migrations: ["./src/shared/infra/typeorm/migrations/*.ts"],
   subscribers: [],
 });
 
 export function createConnection(
-  host = "database_ignite"
+  host = process.env.DATABASE_HOST
 ): Promise<DataSource> {
   console.log(`Connecting to database on ${host}`);
   return AppDataSource.setOptions({ host }).initialize();
